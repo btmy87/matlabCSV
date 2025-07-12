fb93c3d03f9f218a2ed053f6579127c623e21dfc
Brian Magaha <btmy87@gmail.com>
2025-07-12 12:22:30 -0400

    Working on code to parse variable names

--- mexcsv.cpp
+++ mexcsv.cpp
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 #include <string>
+#include <fstream>
 
 class MexFunction : public matlab::mex::Function
 {
@@ -16,6 +17,42 @@ public:
     std::string filename = filename0.toAscii();
     const size_t rowVarNames = inputs[1][0];
     const size_t rowsSkip = inputs[2][0];
+
+    // open file
+    std::ifstream inFile(filename);
+    if (!inFile.is_open())
+      mexPrintError("Could not open file " + filename);
+
+    // read header
+    std::string line;
+    size_t irow = 0;
+    while (irow < rowVarNames && std::getline(inFile, line))
+    {
+      // skip rows up to rowVarNames
+      ++irow;
+    }
+    // data from rowVarNames should be in 'line'
+    std::istringstream lineStream(line);
+    std::vector<matlab::data::CharArray> varNames;
+    std::string tempName;
+    while (std::getline(lineStream, tempName, ','))
+    {
+      varNames.push_back(factory.createCharArray(tempName));
+    }
+
+    // put into output cell array
+    outputs[1] = factory.createCellArray({1, varNames.size()});
+    for (size_t i = 0; i < varNames.size(); ++i)
+    {
+      outputs[1][i] = varNames[i];
+    }
+
+    // read data
+    // just a blank for now
+    outputs[0] = factory.createCellArray({1, 1});
+
+    // close file
+    inFile.close();
   }
 
   void checkArguments(matlab::mex::ArgumentList &outputs, matlab::mex::ArgumentList &inputs)
@@ -28,7 +65,7 @@ public:
 
   void mexPrintError(std::string errMsg)
   {
-    std::string errHeader("Error using mexListDlls:\n");
+    std::string errHeader("Error using mexcsv:\n");
     std::vector<matlab::data::Array> msgArray = {factory.createScalar(errHeader + errMsg)};
     matlabPtr->feval(u"error", 0, msgArray);
   };
